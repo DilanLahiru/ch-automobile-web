66ea38efa63934ecc9bcb3819f3f856a6d260dcd
DilanLahiru <dilanlahiruijse@gmail.com>
2026-03-12 18:55:30 +0530

    fix(baseUrl): correct order of base URL exports for API connection

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,7 +1,7 @@
 // Define the base URL for the API
 //export const baseUrl = "http://212.24.110.146:5000";
-export const baseUrl = "http://192.168.8.101:3000";
-//export const baseUrl = "http://143.20.170.42:3000";
+//export const baseUrl = "http://192.168.8.101:3000";
+export const baseUrl = "http://143.20.170.42:3000";
 
 export const API_PATH = {
   AUTH: {
